e4c0c2ded3cc4a5267ff9bb20095f5cd40c59806
Nisan Ayash <NisanA@radware.com>
2021-07-21 16:16:20 +0300

    start with simple api

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def say_hello(req): 
+    return HttpResponse('Hello world!')
+
+
+def say_goodbye(req): 
+    x = 1
+    y = 2
+    return HttpResponse('Goodbye')
